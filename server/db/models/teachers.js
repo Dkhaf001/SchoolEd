83bcea57eed73e472044f0c78abbaa046b2b54d7
Daniel Khafi <Dkhaf001@gmail.com>
2018-03-07 19:56:08 -0800

    mini function in teachers.js

--- server/db/models/teachers.js
+++ server/db/models/teachers.js
@@ -14,3 +14,9 @@ Teachers.belongsTo(Schools, { foreignKey: 'school_id' });
 Teachers.belongsTo(Classes, { foreignKey: 'class_id' });
 
 module.exports = Teachers;
+
+// let data = await StudentsTeachers.findAll({ where: { teacher_id: 1 } });
+// data.forEach((row) => {
+//   let newData = await StudentsClasses.findAll({ where: { student_id: row.student_id } });
+
+// });
